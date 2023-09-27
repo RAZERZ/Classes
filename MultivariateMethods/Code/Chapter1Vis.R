3105e49b8cd75960294e4e4f267622da9c4660d6
RAZERZ <ramiabouzahra@gmail.com>
2023-09-27 23:48:08 +0200

    Update

--- MultivariateMethods/Code/Chapter1Vis.R
+++ MultivariateMethods/Code/Chapter1Vis.R
@@ -1,16 +1,16 @@
 load("Chapter1Vis.RData")
 library(tcltk)
 X11()
-#hist(Paper$Density)
-#locator(1)
+hist(Paper$Density)
+locator(1)
 plot(Paper$Density, Paper[["Machine direction"]])
 locator(1)
 
 ## Scatter plot
 library(ggplot2)
 library(GGally)
-#ggpairs(Paper)
-#locator(1)
+ggpairs(Paper)
+locator(1)
 
 ## Violin plots
 ggplot(data = Paper, aes(x = Group, y = `Machine direction`)) +
